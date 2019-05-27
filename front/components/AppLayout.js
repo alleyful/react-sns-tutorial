369cb9de926486facd6ea4ed6a2d4874ad3d62d9
alleyful <alleyful@naver.com>
2019-05-28 00:40:53 +0900

    [front] _app.js 추가, grid layout 적용

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import Link from 'next/link'
-import { Menu, Input, Button } from 'antd';
+import { Menu, Input, Button, Row, Col, Card, Avatar } from 'antd';
+import LoginForm from './LoginForm';
+
+const dummy = {
+	nickname: 'alley',
+	Post: [],
+	Followings: [],
+	Followers: [],
+	isLoggedIn: true
+};
 
 const AppLayout = ({ children }) => {
 	return (
@@ -13,7 +22,31 @@ const AppLayout = ({ children }) => {
 				</Menu.Item>
 			</Menu>
 			<Link href="/signup"><a><Button>회원가입</Button></a></Link>
-			{children}
+			<Row>
+				<Col xs={24} md={6}>
+					{dummy.isLoggedIn ? (
+						<Card
+							actions={[
+								<div key="twit">aaa<br/>{dummy.Post.length}</div>,
+								<div key="following">bbb<br/>{dummy.Followings.length}</div>,
+								<div key="followers">ccc<br/>{dummy.Followers.length}</div>
+							]}
+						>
+							<Card.Meta
+								avatar={<Avatar>{dummy.nickname[0]}}</Avatar>}
+								title={dummy.nickname}
+							/>
+							<Link href="./signup"><a><Button>회원가입</Button></a></Link>
+						</Card>
+					) : (
+						<LoginForm />
+					)}
+				</Col>
+				<Col xs={24} md={12}>
+					{children}
+				</Col>
+				<Col xs={24} md={6}>3</Col>
+			</Row>
 		</div>
 	);
 };
--- front/pages/profile.js
+++ front/pages/profile.js
@@ -1,18 +1,9 @@
 import React from 'react';
-import Head from 'next/head';
-import AppLayout from '../components/AppLayout';
 
 const Profile = () => {
 	return (
 		<>
-			<Head>
-				<title>NodeBird</title>
-				<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.19.0/antd.css"/>
-				<script src="https://cdnjs.cloudflare.com/ajax/libs/antd/3.19.0/antd.js"/>
-			</Head>
-			<AppLayout>
-				<div>Profile</div>
-			</AppLayout>
+			<div>Profile</div>
 		</>
 	)
 };
